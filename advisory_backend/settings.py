edbc8e59a523898f2df6f2bd2578e8b3a24c3694
Nandagopal vs <nandagopalvs12@gmail.com>
2023-05-09 12:37:30 +0530

    c1

--- advisory_backend/settings.py
+++ advisory_backend/settings.py
@@ -106,9 +106,19 @@ WSGI_APPLICATION = 'advisory_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-      "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-}
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 
 # Password validation
