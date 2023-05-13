c34d4281eb2a48ae32d03421042c09a361a0421d
Nandagopal vs <nandagopalvs12@gmail.com>
2023-05-13 21:28:28 +0530

    Added filters to ListUsers Endpoint [fix 03]

--- base/api/views.py
+++ base/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from base.models import CustomUser
 from .serializers import ListUserSerializer,UserProfileSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 
 # Create your views here.
@@ -10,7 +11,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 class UserView(generics.ListAPIView):
     queryset= CustomUser.objects.all()
     serializer_class= ListUserSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend,SearchFilter]
     filterset_fields = ['batch']
     search_fields = ['username']
 
