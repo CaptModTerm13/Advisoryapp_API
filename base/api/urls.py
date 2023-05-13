9e24be7bb732be4d44799cbf662bd646e380c8f2
Nandagopal vs <nandagopalvs12@gmail.com>
2023-05-13 20:48:37 +0530

    Added filters to ListUsers Endpoint

--- base/api/urls.py
+++ base/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import UserView,UserProfileView,UpdateUser
 
 urlpatterns=[
-    path("",UserView.as_view(),name="users"),
+    path("",UserView.as_view(),name="usersList"),
     path("user/<username>", UserProfileView.as_view(), name="user_detail"),
     path("update/<username>",UpdateUser.as_view(),name="update-view")
 ]
--- base/api/views.py
+++ base/api/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from rest_framework import generics
 from base.models import CustomUser
 from .serializers import ListUserSerializer,UserProfileSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 # Create your views here.
 
 class UserView(generics.ListAPIView):
     queryset= CustomUser.objects.all()
     serializer_class= ListUserSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['username', 'batch']
 
 class UserProfileView(generics.RetrieveUpdateDestroyAPIView):
    queryset=CustomUser.objects.all()
