27c83130c8584d0cda93b5e8aa49f21a0163afd7
Nandagopal vs <nandagopalvs12@gmail.com>
2023-05-14 10:52:51 +0530

    fix[03]

--- base/models.py
+++ base/models.py
@@ -21,9 +21,9 @@ class CustomUser(AbstractUser):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES,default="M")
     batch = models.CharField(max_length=7,choices=BATCH_CHOICES,default="Default")
     role = models.CharField(max_length=7,choices=ROLES,default="S")
-    phone_no=models.CharField(max_length=20,null=True);
+    phone_no=models.CharField(null=True);
     guardian=models.CharField(max_length=40,null=True);
-    guardian_no=models.CharField(max_length=20,null=True);
+    guardian_no=models.CharField(null=True);
     teacher_remarks=models.CharField(null=True);
     # add additional fields in here
 
